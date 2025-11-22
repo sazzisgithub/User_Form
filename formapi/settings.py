4f7f81c8f3ee5f1e5679afd7fdf6ef26e556e5ce
Sajal <shrivastavas1510@gmai.com>
2025-11-23 02:37:12 +0530

    DB Udated

--- formapi/settings.py
+++ formapi/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'formapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb',
+        'USER': 'postgres',
+        'PASSWORD': 'Myhtml#123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        },
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
