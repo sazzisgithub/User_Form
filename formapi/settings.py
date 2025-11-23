b46f1c074aedbe65b3135dddc858c0681fd0b8bd
Sajal <shrivastavas1510@gmai.com>
2025-11-23 20:30:32 +0530

    Updated settings.py

--- formapi/settings.py
+++ formapi/settings.py
@@ -133,4 +133,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOWED_ORIGINS = [
+    "https://frontend-hgqi.onrender.com",
+]
+
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
